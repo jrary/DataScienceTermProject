1abc9c6411616eb7306a2c51d8156201171062f6
jrary <jrary@naver.com>
2023-05-18 19:40:36 +0900

    Combine seoulgrandpark visitor, atmosphere data

--- src/dataset.py
+++ src/dataset.py
@@ -31,7 +31,7 @@ def mergeForecastDataset():
             print(df)
             df = df.loc[:, ['timestamp', 'value']]
             allYears = pd.concat([df, allYears])
-        allYears.to_csv("./src/assets/output/부림동_" + fileName + ".csv")
+        allYears.to_csv("assets/output/부림동_" + fileName + '.csv')
 
 
 # Visitor
@@ -44,7 +44,7 @@ def makeVisitor():
     for year in years:
         try:
             data = pd.read_excel(
-                './src/assets/SGPyear/Visitor_'+year+'.xlsx', skiprows=2, usecols=[0, 5])
+                'assets/input/SGPyear/Visitor_'+year+'.xlsx', skiprows=2, usecols=[0, 5])
         except FileNotFoundError:
             print("Error: File not found!")
             continue
@@ -57,26 +57,28 @@ def makeVisitor():
         data.columns = ['date', 'visitor']
         visitor = pd.concat([data, visitor])
     visitor.reset_index(inplace=True, drop=True)
-    visitor.to_csv("./src/assets/output/visitors.csv")
+    visitor.to_csv("assets/output/visitors.csv")
 
 def makeAtmosphere():
-    years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014',
-            '2015', '2016', '2017', '2018', '2019', '2020']
+    years = ['2008' , '2009', '2010', '2011', '2012', '2013', '2014',
+         '2015', '2016', '2017', '2018', '2019' ]
     years.reverse()
     atmosphere = pd.DataFrame()
     for year in years:
         try:
-            data = pd.read_csv(
-                './src/assets/Atmosphere/Atmosphere_'+year+'.csv', encoding='cp949')
+            if year == '2018' or year == '2019':
+                data = pd.read_csv('assets/input/Atmosphere/Atmosphere_'+year+'.csv', encoding='cp949', usecols=range(1,12))
+            else:
+                data = pd.read_csv('assets/input/Atmosphere/Atmosphere_'+year+'.csv', encoding='cp949')
             data.columns = ['city', 'town', 'established', 'position', 'timestamp', 
-                            'sulfur_dioxide', 'carbon monoxide', 'ozone', 'nitrogen_dioxide', 
-                            'fine_dust_pm10', 'fine_dust_pm2.5']
+                                'sulfur_dioxide', 'carbon_monoxide', 'ozone', 'nitrogen_dioxide', 
+                                'fine_dust_pm10', 'fine_dust_pm2.5']
             df = pd.DataFrame(data)
+            # Extract only data close to Seoul Grand Park
+            df = df.loc[(df['city']=='Gwacheon') & (df['established']!=1991)]
             # Unnecessary data: "설치년도", "측정망 정보"   
             # Unavailable data (many missing data): "미세먼지PM2.5농도값(μg/m³)"
             df.drop(['established', 'position', 'fine_dust_pm2.5'], axis=1, inplace=True)
-            # Extract only data close to Seoul Grand Park
-            df = df.loc[(df['city']=="과천시") & (df['town']=="과천동")]
             # Unnecessary data (because all data values are same): "시군명", "측정소명"
             df.drop(['city', 'town'], axis=1, inplace=True)
             # Split the data with Date/Time
@@ -92,6 +94,6 @@ def makeAtmosphere():
             print("Error:", str(e))
             continue
 
-        atmosphere = pd.concat([data, atmosphere])
+        atmosphere = pd.concat([df, atmosphere])
     atmosphere.reset_index(inplace=True, drop=True)
-    atmosphere.to_csv("./src/assets/output/atmosphere.csv", encoding='cp949')
+    atmosphere.to_csv("assets/output/atmosphere.csv", encoding='cp949')
